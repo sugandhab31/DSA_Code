fbc30bfdd6b329073bbdfc9d48cb77846331ad9a
sugandhab31 <sugandhabansal31@gmail.com>
2024-04-13 21:15:46 +0530

    inital commit

---
+++ src/leetcode_75/MergeStringsAlternately.java
@@ -0,0 +1,15 @@
+package leetcode_75;
+
+/**
+ * 1768. Merge Strings Alternately
+ * You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. 
+ * If a string is longer than the other, append the additional letters onto the end of the merged string.
+ * Return the merged string.
+ */
+
+public class MergeStringsAlternately {
+
+    public static void main(String[] args) {
+        
+    }
+}
