5424ff6533ac563ba984c05d425852baa1ac87ea
sugandhab31 <sugandhabansal31@gmail.com>
2024-04-13 23:31:54 +0530

    MergeStringsAlternately

--- src/leetcode_75/MergeStringsAlternately.java
+++ src/leetcode_75/MergeStringsAlternately.java
@@ -7,9 +7,36 @@ package leetcode_75;
  * Return the merged string.
  */
 
-public class MergeStringsAlternately {
-
+ class Solution {
+    /**
+     * @param word1
+     * @param word2
+     * @return
+     */
     public static void main(String[] args) {
-        
+        System.out.println(mergeAlternately("abcd","xyz123"));
     }
+
+    public static String mergeAlternately(String word1, String word2) {
+        String result = new String();
+        int i = 0;
+        int j = 0;
+        while (i<word1.length() && j<word2.length()) {            
+            result = result+word1.charAt(i)+word2.charAt(j);
+            i++; 
+            j++;        
+        }
+        while (i!=word1.length()) {
+            result = result+word1.charAt(i);
+            i++;
+        }
+        while (j!=word2.length()) {            
+            result = result+word2.charAt(j);
+            j++;
+        }
+        
+
+    return result;
+    }
+    
 }
